140c1d14e020d4f6e22390aa59ffb9e0da6ef2e9
Alexandre <aleaxe7@gmail.com>
2022-09-28 22:24:36 -0300

    Fix board size

--- scripts.js
+++ scripts.js
@@ -7,6 +7,7 @@ for (i = 1; i <= 16; i++) {
     for(j = 1; j <= 16; j++) {
         let square = document.createElement("div");
         square.textContent = `${i}`;
+        square.style.cssText = "border: 1px; border-style: solid; border-color: rgb(255, 255, 255);"
         row.appendChild(square)
     }
 }
