7b4f8e4e40ca87d0544cefe95bfd097ec885c665
Alexandre <aleaxe7@gmail.com>
2022-10-03 20:45:51 -0300

    Added function to change background color

--- scripts.js
+++ scripts.js
@@ -1,14 +1,39 @@
 const board = document.querySelector(".board");
 
-for (i = 1; i <= 16; i++) {
+for (i = 1; i <= 32; i++) {
     let row = document.createElement("div");
-    row.style.cssText = "background-color: rgb(0, 0, 0);";
+    row.style.cssText = "background-color: black; display:flex; flex-direction: column; flex:1;";
     board.appendChild(row);
-    for(j = 1; j <= 16; j++) {
+    for(j = 1; j <= 32; j++) {
         let square = document.createElement("div");
-        square.textContent = `${i}`;
-        square.style.cssText = "border: 1px; border-style: solid; border-color: rgb(255, 255, 255);"
-        row.appendChild(square)
+        square.classList.add("square");
+        square.style.cssText = "width:5px; height: 5px;";
+        row.appendChild(square);
     }
 }
 
+function changeBoardSize(size) {
+    for (i = 1; i <= size; i++) {
+        let row = document.createElement("div");
+        row.style.cssText = "background-color: white; display:flex; flex-direction: column; flex:1;";
+        board.appendChild(row);
+        for(j = 1; j <= size; j++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.style.cssText = "width:5px; height: 5px;"
+            row.appendChild(square)
+        }
+    }    
+}
+
+function randomColor () {
+    let color = Math.floor(Math.random()*16777215).toString(16);
+    return "#" + color;
+}
+
+toColor = document.querySelectorAll(".square");
+toColor.forEach(toColor => {
+    toColor.addEventListener("mouseover", () => {
+        toColor.style.cssText = `width: 5px; height: 5px; background-color: ${randomColor()};`
+    })
+})
