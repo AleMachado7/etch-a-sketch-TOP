585d0664e4f5b358b782e5fcb6e4251397fa9265
Alexandre <aleaxe7@gmail.com>
2022-10-05 22:37:24 -0300

    Change game board sizing  function, change drawing color function

--- scripts.js
+++ scripts.js
@@ -1,64 +1,66 @@
+const BOARD_FRAME = document.querySelector(".game-board");
+
+//Main Function
 function DrawBoard(size) {
-    let boardFrame = document.createElement("div").classList.add("board");
-    let gameBoard = document.querySelector(".game-board")
-    gameBoard.appendChild(boardFrame)
-}
-
-let boardFrame = document.createElement("div");
-boardFrame.classList.add("board")
-let gameBoard = document.querySelector(".game-board")
-gameBoard.appendChild(boardFrame)
-
-
-const board = document.querySelector(".board");
-
-for (i = 0; i < board.clientHeight/10; i++) {
-    let row = document.createElement("div");
-    row.style.cssText = "background-color: black; display:flex; flex-direction: column;"
-    board.appendChild(row);
-    for(j = 0; j <= board.clientWidth/10; j++) {
-        let square = document.createElement("div");
-        square.classList.add("square");
-        square.style.cssText = "width: 10px; height: 10px;";
-        row.appendChild(square);
+    //check for elements to remove in case of resizing
+    while (BOARD_FRAME.firstChild) {
+        BOARD_FRAME.removeChild(BOARD_FRAME.lastChild);
     }
-}
+    //Draw elements on the board
 
-function DrawBoard(size) {
-    board.style.height = `${size}px`;
-    board.style.width = `${size}px`;
-    gameBoard.removeChild(boardFrame)
-    gameBoard.appendChild(boardFrame)
+    let gameBoard = document.createElement("div");
+    gameBoard.classList.add("board");
+    BOARD_FRAME.appendChild(gameBoard);
 
-    for (i = 0; i < board.clientHeight/10; i++) {
+    for (i = 0; i < size; i++) {
         let row = document.createElement("div");
-        row.style.cssText = "background-color: black; display:flex; flex-direction: column;"
-        board.appendChild(row);
-        for(j = 1; j <= board.clientWidth/10; j++) {
+        row.style.cssText = "background-color: grey; display:flex; flex-direction: column;"
+        gameBoard.appendChild(row);
+        for(j = 0; j < size; j++) {
             let square = document.createElement("div");
             square.classList.add("square");
-            square.style.cssText = "width: 10px; height: 10px;";
+            square.style.cssText = `width: ${BOARD_FRAME.clientWidth/size}px; height: ${BOARD_FRAME.clientHeight/size}px;`;
             row.appendChild(square);
         }
     }
-    return ;
 }
 
+
+// Initiate board with bigger size as default
+DrawBoard(10) 
+
+
+//function to return HEX color randomly
 function randomColor () {
     let color = Math.floor(Math.random()*16777215).toString(16);
     return "#" + color;
 }
 
-let gameSize = document.querySelectorAll(".game-size");
 
-gameSize.forEach(gameSize => {
-    
-})
-
-
-let toColor = document.querySelectorAll(".square");
-toColor.forEach(toColor => {
-    toColor.addEventListener("mouseover", () => {
-        toColor.style.cssText = `width: 10px; height: 10px; background-color: ${randomColor()};`
+// change board pixel sizes
+let squareSize = document.querySelectorAll(".game-size");
+squareSize.forEach(squareSize => {
+    squareSize.addEventListener("click", () => {
+        DrawBoard(squareSize.value)
     })
 })
+
+// set square drawing color
+function drawingColor(color) {
+    let toColor = document.querySelectorAll(".square");
+
+    if (color === "random") {      
+        toColor.forEach(toColor => {
+            toColor.addEventListener("mouseover", () => {
+                toColor.style.backgroundColor = randomColor();
+            })
+        })  
+    }
+
+    toColor.forEach(toColor => {
+        toColor.addEventListener("mouseover", () => {
+            toColor.style.backgroundColor = color;
+        })
+    })
+}
+
